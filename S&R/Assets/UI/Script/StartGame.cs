8c856949e945099517a517ae76942dafcb41b60b
TangYikChin <66994561+TangYikChin@users.noreply.github.com>
2021-12-03 21:45:41 +0800

    Main UI Done

--- S&R/Assets/UI/Script/StartGame.cs
+++ S&R/Assets/UI/Script/StartGame.cs
@@ -8,7 +8,7 @@ public class StartGame : MonoBehaviour
 {
    public void playGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + "");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
